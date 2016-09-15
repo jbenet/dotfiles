097be503e2606d28174137f2d0cbebfdeba9f5d2
jbenet <juan@benet.ai>
2016-09-15 18:22:15 -0400

    mjolnir stuff

--- install-tools.sh
+++ install-tools.sh
@@ -11,6 +11,13 @@ brew_install zsh
 brew_install git
 brew_install wget
 brew_install curl
+brew_install lua
+
+# for mjolnir
+echo 'rocks_servers = { "http://rocks.moonscript.org" }' >> /usr/local/etc/luarocks52/config-5.2.lua
+luarocks install mjolnir.application
+luarocks install mjolnir.hotkey
+luarocks install mjolnir.bg.grid
 
 # ln subl
 which subl || ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
@@ -18,3 +25,4 @@ which subl || ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/s
 # other
 which go || echo "TODO: install golang"
 which ipfs || echo "TODO: install ipfs"
+echo "TODO: chmod -R $(whoami) /usr/local"
